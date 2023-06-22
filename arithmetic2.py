413fb13d7bf494a4c2713c40b61e1dfe7bbe2b18
maqswood <maqswood1567@gmail.com>
2023-06-22 11:53:20 +0530

    class3

---
+++ arithmetic2.py
@@ -0,0 +1,17 @@
+x=10
+y=12
+
+add=x+y
+sub=x-y
+mul=x*y
+div=x/y
+intdiv=x//y
+mod=x%y
+power=x**y
+print("sum is",add)
+print("substraction  is",sub)
+print("mul is",mul)
+print("div is",div)
+print("intger divition is",intdiv)
+print("mod is",mod)
+print("power is",power)
---
+++ logical.py
@@ -0,0 +1,6 @@
+x=10
+y=8
+
+print("using and",x>y and x<y)
+print("using and",x>y or x<y)
+print("using and",not (x>y and x<y))
