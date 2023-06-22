f5a04b5460aea88919606146cc31a25a110388cb
maqswood <maqswood1567@gmail.com>
2023-06-22 12:06:36 +0530

    class3

---
+++ idea_testing.py
@@ -0,0 +1,10 @@
+print("enter ur 1st number")
+x: int(input())
+print("enter ur 2nd number")
+y: int = int(input())
+
+sum = x + y
+sub = x - y
+print("ur sum is ", sum)
+
+print("ur substracton is ", sub)
--- logical.py
+++ logical.py
@@ -2,5 +2,5 @@ x=10
 y=8
 
 print("using and",x>y and x<y)
-print("using and",x>y or x<y)
-print("using and",not (x>y and x<y))
+print("using or",x>y or x<y)
+print("using not",not (x>y and x<y))
